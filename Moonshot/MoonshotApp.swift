093d3a4eb06a493df1e292bbfcfec3d76eaabf36
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-04-06 18:03:55 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Carlos Román Alcaide on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
