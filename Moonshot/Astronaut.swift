093339f71a34c8cdeb9e03b70e135019f8bcebaa
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-04-08 23:03:56 +0200

    First commit
    - Decoder added
    - Data structs added
    - Basic layout added

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Carlos Román Alcaide on 8/4/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
